cc4c0bb9c265b2316d0aec6cb5dcc56367fd8073
Unnati Gandhi <gandhiunnati611@gmail.com>
2024-04-09 11:57:35 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import IconButton from "@mui/material/IconButton";
+import RemoveIcon from "@mui/icons-material/Remove";
+import AddIcon from "@mui/icons-material/Add";
 
 function App() {
+  const [count, setCount] = useState(0);
+  // const [count1, setCount1] = useState("unnu");
+  const onaddClick = () => {
+    console.log("btnclick");
+    if (count >= 10) {
+      alert("value is not more than 10");
+    } else {
+      setCount(count + 1);
+    }
+
+    // setCount1(setCount1);
+  };
+  const onRemoveClick = () => {
+    console.log("btnclick");
+    console.log("btnclick");
+    if (count <= 0) {
+      alert("value is not less than 0");
+    } else {
+      setCount(count - 1);
+    }
+
+    // setCount1(setCount1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <IconButton aria-label="add" onClick={onaddClick} size="large">
+        <AddIcon fontSize="inherit" />
+      </IconButton>
+      <h1>{count}</h1>
+      <IconButton aria-label="remove" onClick={onRemoveClick} size="large">
+        <RemoveIcon fontSize="inherit" />
+      </IconButton>
     </div>
   );
 }
